e37b0177f263ed27c4a8492424c3d4be76e95ece
Javier3921 <108969161+Javier3921@users.noreply.github.com>
2022-09-15 20:37:35 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Te quiero mucho culuna <3 psdt: Quiero manosear tus chichis') 
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
+
+
